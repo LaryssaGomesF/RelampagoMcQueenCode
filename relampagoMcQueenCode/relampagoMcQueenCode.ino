b9dd32008f91d645a96ce1f868b2cfcd7d68b2b7
Laryssa Fagundes <laryssa.agomesfagundes@gmail.com>
2023-05-07 21:11:09 -0300

    update

--- relampagoMcQueenCode/relampagoMcQueenCode.ino
+++ relampagoMcQueenCode/relampagoMcQueenCode.ino
@@ -19,7 +19,12 @@
 #define M3_M4_IN2 8
 #define PWM_M3_M4 6
 
-
+int minValues[8] = { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
+int maxValues[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int sensor_values[8];
+int lastError = 0;
+const int MAX_SPEED = 255;
+const int HALF_SPEED = 150;
 
 void setupPin() {
   pinMode(s0, INPUT);
@@ -38,21 +43,47 @@ void setupPin() {
   pinMode(PWM_M3_M4, OUTPUT);
 }
 
-void calibracao() {
+int getPosition() {
+  return (3 * sensorNormalization(1) + 2 * sensorNormalization(2) + 1 * sensorNormalization(3) - 1 * sensorNormalization(4) - 2 * sensorNormalization(5) - 3 * sensorNormalization(6));
 }
 
-void setup() {
-  setupPin();
-  calibracao();
+int sensorNormalization(int position) {
+  int result = (((sensor_values[position] - minValues[position]) / (maxValues[position] - minValues[position])) * 1000);
+  return result;
 }
 
-unsigned int sensors[8];
-void loop() {
+void getMinAndMaxValues() {
 
-  int position = readLine(sensors[8]);
+  MotorsRight(255);                  //editar esse valor depois
+  MotorsLeft(0);                     //editar esse valor depois
+  for (int i = 0; i < 80000; i++) {  //editar esse valor depois
+    readSensors();
+    for (int i = 0; i < 8; i++) {
+      if (sensor_values[i] < minValues[i]) {
+        minValues[i] = sensor_values[i];
+      }
+      if (sensor_values[i] > maxValues[i]) {
+        maxValues[i] = sensor_values[i];
+      }
+    }
+  }
+
+  MotorsRight(0);                    //editar esse valor depois
+  MotorsLeft(255);                   //editar esse valor depois
+  for (int i = 0; i < 80000; i++) {  //editar esse valor depois
+    readSensors();
+    for (int i = 0; i < 8; i++) {
+      if (sensor_values[i] < minValues[i]) {
+        minValues[i] = sensor_values[i];
+      }
+      if (sensor_values[i] > maxValues[i]) {
+        maxValues[i] = sensor_values[i];
+      }
+    }
+  }
 }
 
-int readLine(int *sensor_values) {
+void readSensors() {
   sensor_values[0] = { analogRead(s0) };
   sensor_values[1] = { analogRead(s1) };
   sensor_values[2] = { analogRead(s2) };
@@ -61,38 +92,53 @@ int readLine(int *sensor_values) {
   sensor_values[5] = { analogRead(s5) };
   sensor_values[6] = { analogRead(s6) };
   sensor_values[7] = { analogRead(s7) };
-  int _position = 0;
-  return _position;
 }
 
-void MotorsRight(int pwm) {
+int calculateError(int position) {
+  return position - 3000;
+}
 
-  if (pwm == 0) {
-    digitalWrite(M1_M2_IN1, LOW);
-    digitalWrite(M1_M2_IN2, LOW);
-  } else if (pwm < 0) {
-    analogWrite(PWM_M1_M2, -pwm);
-    digitalWrite(M1_M2_IN1, HIGH);
-    digitalWrite(M1_M2_IN2, LOW);
+
+void setup() {
+  setupPin();
+  getMinAndMaxValues();
+  delay(10000);
+}
+
+void loop() {
+  readSensors();
+  int position = getPosition();
+  int error = calculateError(position);
+  int speedDifference = error / 2 + 8 * (error - lastError);
+  lastError = error;
+  int m1Speed = HALF_SPEED + speedDifference;
+  int m2Speed = HALF_SPEED - speedDifference;
+
+  MotorsRight(m1Speed);
+  MotorsLeft(m2Speed);
+}
+
+
+void MotorsRight(int pwm) {
+  digitalWrite(M1_M2_IN1, HIGH);
+  digitalWrite(M1_M2_IN2, LOW);
+  if (pwm < 0) {
+    analogWrite(PWM_M1_M2, HALF_SPEED);
+  } else if (pwm > 255) {
+    analogWrite(PWM_M1_M2, MAX_SPEED);
   } else {
     analogWrite(PWM_M1_M2, pwm);
-    digitalWrite(M1_M2_IN1, LOW);
-    digitalWrite(M1_M2_IN2, HIGH);
   }
 }
 
 void MotorsLeft(int pwm) {
-
-  if (pwm == 0) {
-    digitalWrite(M3_M4_IN1, LOW);
-    digitalWrite(M3_M4_IN2, LOW);
-  } else if (pwm < 0) {
-    analogWrite(PWM_M3_M4, -pwm);
-    digitalWrite(M3_M4_IN1, HIGH);
-    digitalWrite(M3_M4_IN2, LOW);
+  digitalWrite(M3_M4_IN1, HIGH);
+  digitalWrite(M3_M4_IN2, LOW);
+  if (pwm < 0) {
+    analogWrite(PWM_M3_M4, HALF_SPEED);
+  } else if (pwm > 255) {
+    analogWrite(PWM_M3_M4, MAX_SPEED);
   } else {
     analogWrite(PWM_M3_M4, pwm);
-    digitalWrite(M3_M4_IN1, LOW);
-    digitalWrite(M3_M4_IN2, HIGH);
   }
 }
