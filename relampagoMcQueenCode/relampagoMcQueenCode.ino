6a72290fd8cc566f1c240f1aec6b7fe2fa9728a9
Laryssa Gomes <82814498+LaryssaGomesF@users.noreply.github.com>
2023-06-09 17:56:01 -0300

    FUNCIONAAAAAAAAAA

--- relampagoMcQueenCode/relampagoMcQueenCode.ino
+++ relampagoMcQueenCode/relampagoMcQueenCode.ino
@@ -16,14 +16,22 @@
 //MOTOR LEFT
 #define M3_M4_IN2 7
 #define M3_M4_IN1 8
-#define PWM_M3_M4 6
+#define PWM_M3_M4 9
+
+
 
 int minValues[8] = { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
 int maxValues[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 int sensor_values[8];
 int lastError = 0;
-const int MAX_SPEED = 180;
-const int HALF_SPEED = 120;
+int integral = 0;
+const int MAX_SPEED = 255;
+int HALF_SPEED = 210;
+int MIN_SPEED = 150;
+const float KP = 6.0;
+const float C1 = 1.0;
+const float C2 = 2.0;
+const float C3 = 3.0;
 
 void setupPin() {
   Serial.begin(9600);
@@ -43,18 +51,11 @@ void setupPin() {
   pinMode(PWM_M3_M4, OUTPUT);
 }
 
-int getPosition() {
-  return (3 * sensorNormalization(1) + 2 * sensorNormalization(2) + 1 * sensorNormalization(3) - 1 * sensorNormalization(4) - 2 * sensorNormalization(5) - 3 * sensorNormalization(6));
-}
-
-int sensorNormalization(int position) {
-  float result =  ((((sensor_values[position] - minValues[position]) * 1.0) / (maxValues[position] - minValues[position])) * 1000);
-  return result;
-}
-
 void getMinAndMaxValues() {
-  //editar esse valor depois
-  for (int i = 0; i < 15000; i++) {  //editar esse valor depois
+
+  MotorsLeft(-250);
+  MotorsRight(250);
+  for (int i = 0; i < 3000; i++) {
     readSensors();
     for (int i = 0; i < 8; i++) {
       if (sensor_values[i] < minValues[i]) {
@@ -65,22 +66,45 @@ void getMinAndMaxValues() {
       }
     }
   }
+  MotorsLeft(250);
+  MotorsRight(-250);
+  for (int i = 0; i < 3000; i++) {
+    readSensors();
+    for (int i = 0; i < 8; i++) {
+      if (sensor_values[i] < minValues[i]) {
+        minValues[i] = sensor_values[i];
+      }
+      if (sensor_values[i] > maxValues[i]) {
+        maxValues[i] = sensor_values[i];
+      }
+    }
+  }
+  MotorsLeft(0);
+  MotorsRight(0);
+}
+
+float getPosition() {
+  return (C3 * (sensorNormalization(1)) + C2 * (sensorNormalization(2)) + C1 * (sensorNormalization(3)) + -C1 * (sensorNormalization(4)) + -C2 * (sensorNormalization(5)) + -C3 * (sensorNormalization(6)));
+}
+
+float sensorNormalization(int pos) {
+  float result = ((sensor_values[pos] - minValues[pos]) / (1.0 * (maxValues[pos] - minValues[pos])));
+  return 1.0 - result;
 }
 
 void readSensors() {
-  sensor_values[0] = { analogRead(s0) };
-  sensor_values[1] = { analogRead(s1) };
-  sensor_values[2] = { analogRead(s2) };
-  sensor_values[3] = { analogRead(s3) };
-  sensor_values[4] = { analogRead(s4) };
-  sensor_values[5] = { analogRead(s5) };
-  sensor_values[6] = { analogRead(s6) };
-  sensor_values[7] = { analogRead(s7) };
-  
+  sensor_values[0] = analogRead(s0);
+  sensor_values[1] = analogRead(s1);
+  sensor_values[2] = analogRead(s2);
+  sensor_values[3] = analogRead(s3);
+  sensor_values[4] = analogRead(s4);
+  sensor_values[5] = analogRead(s5);
+  sensor_values[6] = analogRead(s6);
+  sensor_values[7] = analogRead(s7);
 }
 
-int calculateError(int position) {
-  return ((position - 3000) / (1000.0));
+int calculateError(float pos) {
+  return 10 * pos;
 }
 
 
@@ -88,64 +112,108 @@ void setup() {
   Serial.begin(9600);
   setupPin();
   getMinAndMaxValues();
-  for (int i = 0; i < 8; i++) {
-    Serial.println("Min value $i:     ");
-    Serial.println(minValues[i]);
-  }
-  for (int i = 0; i < 8; i++) {
-    Serial.println("Max value $i:     ");
-    Serial.println(maxValues[i]);
-  }
+  Serial.print("Ready");
   Serial.println();
+  delay(3000);
 }
+
+int flag = 0;
+int contD = 0;
 
 void loop() {
 
   readSensors();
-  int position = getPosition();
-  // int error = calculateError(position);
-  int speedDifference = position/2;
-  // + 18.5* (position - lastError);
-  lastError = position;
+
+  float pos = getPosition();
+  int error = calculateError(pos);
+  int speedDifference = KP * error;  // + 2*(error - lastError) + 0*integral;
+  lastError = error;
+  integral += error;
+
   int m1Speed = HALF_SPEED + speedDifference;
   int m2Speed = HALF_SPEED - speedDifference;
-
-  Serial.println("POSICAO:               ");
-  Serial.println(position);
-  Serial.println("ERROR:               ");
-   //Serial.println(error);
-
-  Serial.println("M1SPEED:               ");
-  Serial.println(m1Speed);
-  Serial.println("M2SPEED:               ");
-  Serial.println(m2Speed);
 
 
   MotorsRight(m1Speed);
   MotorsLeft(m2Speed);
 
+  /*Serial.print("sensor  1    :");
+  Serial.print(sensor_values[1]);
+  Serial.println();
+
+  Serial.print("sensor  2    :");
+  Serial.print(sensor_values[2]);
+  Serial.println();
+
+  Serial.print("sensor  3    :");
+  Serial.print(sensor_values[3]);
+  Serial.println();
+
+  Serial.print("sensor  4    :");
+  Serial.print(sensor_values[4]);
+  Serial.println();
+
+  Serial.print("sensor  5   :");
+  Serial.print(sensor_values[5]);
+  Serial.println();
+
+  Serial.print("sensor  6   :");
+  Serial.print(sensor_values[6]);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+  delay(5000);*/
+
+
+  /*
+  Serial.print("P:");
+  Serial.print(pos);
+  Serial.print(" E:");
+  Serial.print(error);
+  Serial.print(" D:");
+  Serial.print(speedDifference);
+  Serial.print(" M1:");
+  Serial.print(m1Speed);
+  Serial.print(" M2:");
+  Serial.print(m2Speed);
+  Serial.println();
+
+ */
+
 }
 
 void MotorsRight(int pwm) {
-  digitalWrite(M1_M2_IN1, HIGH);
-  digitalWrite(M1_M2_IN2, LOW);
+
   if (pwm < 0) {
-    analogWrite(PWM_M1_M2, HALF_SPEED);
-  } else if (pwm > 255) {
-    analogWrite(PWM_M1_M2, MAX_SPEED);
+    digitalWrite(M1_M2_IN1, LOW);
+    digitalWrite(M1_M2_IN2, HIGH);
   } else {
-    analogWrite(PWM_M1_M2, pwm);
+    digitalWrite(M1_M2_IN1, HIGH);
+    digitalWrite(M1_M2_IN2, LOW);
+  }
+  if (abs(pwm) > MAX_SPEED) {
+    analogWrite(PWM_M1_M2, MAX_SPEED);
+  } else if (abs(pwm) < MIN_SPEED) {
+    analogWrite(PWM_M1_M2, abs(MIN_SPEED));
+  } else {
+    analogWrite(PWM_M1_M2, abs(pwm));
   }
 }
 
 void MotorsLeft(int pwm) {
-  digitalWrite(M3_M4_IN1, HIGH);
-  digitalWrite(M3_M4_IN2, LOW);
+
   if (pwm < 0) {
-    analogWrite(PWM_M3_M4, HALF_SPEED);
-  } else if (pwm > 255) {
-    analogWrite(PWM_M3_M4, MAX_SPEED);
+    digitalWrite(M3_M4_IN1, LOW);
+    digitalWrite(M3_M4_IN2, HIGH);
   } else {
-    analogWrite(PWM_M3_M4, pwm);
+    digitalWrite(M3_M4_IN1, HIGH);
+    digitalWrite(M3_M4_IN2, LOW);
+  }
+  if (abs(pwm) > MAX_SPEED) {
+    analogWrite(PWM_M3_M4, MAX_SPEED);
+  } else if (abs(pwm) < MIN_SPEED) {
+    analogWrite(PWM_M3_M4, abs(MIN_SPEED));
+  } else {
+    analogWrite(PWM_M3_M4, abs(pwm));
   }
 }
