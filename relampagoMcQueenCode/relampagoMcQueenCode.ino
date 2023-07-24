32101259360f148c8f89ca5defd0adc7a0325637
Laryssa Gomes <82814498+LaryssaGomesF@users.noreply.github.com>
2023-07-24 15:20:15 -0300

    Rcx code  relampagoMcQueenCode.ino

--- relampagoMcQueenCode/relampagoMcQueenCode.ino
+++ relampagoMcQueenCode/relampagoMcQueenCode.ino
@@ -8,14 +8,16 @@
 #define s6 A6
 #define s7 A7
 
+#define LED 12
+
 //MOTOR RIGHT
-#define M1_M2_IN1 2
-#define M1_M2_IN2 4
+#define M1_M2_IN1 4
+#define M1_M2_IN2 2
 #define PWM_M1_M2 3
 
 //MOTOR LEFT
-#define M3_M4_IN2 7
-#define M3_M4_IN1 8
+#define M3_M4_IN2 8
+#define M3_M4_IN1 7
 #define PWM_M3_M4 9
 
 
@@ -25,11 +27,13 @@ int maxValues[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 int sensor_values[8];
 int lastError = 0;
 int integral = 0;
+bool atual = false;
+bool anterior = false;
 const int MAX_SPEED = 255;
-int HALF_SPEED = 110;
+int HALF_SPEED = 120;
 int MIN_SPEED = 90;
-const float KP = 8.0;
-const float KD = 30.0;
+const float KP = 10.0;
+const float KD = 15.0;
 const float C1 = 1.0;
 const float C2 = 2.0;
 const float C3 = 3.0;
@@ -50,12 +54,13 @@ void setupPin() {
   pinMode(M3_M4_IN2, OUTPUT);
   pinMode(PWM_M1_M2, OUTPUT);
   pinMode(PWM_M3_M4, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void getMinAndMaxValues() {
 
-  MotorsLeft(-HALF_SPEED);
-  MotorsRight(HALF_SPEED);
+  MotorsLeft(-160);
+  MotorsRight(160);
   for (int i = 0; i < 3000; i++) {
     readSensors();
     for (int i = 0; i < 8; i++) {
@@ -67,8 +72,8 @@ void getMinAndMaxValues() {
       }
     }
   }
-  MotorsLeft(HALF_SPEED);
-  MotorsRight(-HALF_SPEED);
+  MotorsLeft(160);
+  MotorsRight(-160);
   for (int i = 0; i < 3000; i++) {
     readSensors();
     for (int i = 0; i < 8; i++) {
@@ -134,10 +139,32 @@ void loop() {
   int m2Speed = HALF_SPEED - speedDifference;
 
 
-  MotorsRight(m1Speed);
-  MotorsLeft(m2Speed);
+  if (sensor_values[0] < 800 && anterior == false) {
+    anterior = true;
+    digitalWrite(LED, HIGH);
+  }else if(sensor_values[0] > 800 && anterior == true){
+    anterior = false;
+    digitalWrite(LED, LOW);
+    contD++; 
+  }
 
-  /*Serial.print("sensor  1    :");
+
+
+  if (contD >= 10) {
+    motorStop();
+  } else {
+    MotorsRight(m1Speed);
+    MotorsLeft(m2Speed);
+  }
+
+  /*Serial.print("sensor  0    :");
+  Serial.print(sensor_values[0]);
+  Serial.println();
+  delay(5000);
+  
+  
+
+  Serial.print("sensor  1    :");
   Serial.print(sensor_values[1]);
   Serial.println();
 
@@ -160,13 +187,15 @@ void loop() {
   Serial.print("sensor  6   :");
   Serial.print(sensor_values[6]);
   Serial.println();
+
+  Serial.print("sensor  7   :");
+  Serial.print(sensor_values[7]);
+  Serial.println();
   Serial.println();
   Serial.println();
   delay(5000);*/
 
-
-  /*
-  Serial.print("P:");
+  /*Serial.print("P:");
   Serial.print(pos);
   Serial.print(" E:");
   Serial.print(error);
